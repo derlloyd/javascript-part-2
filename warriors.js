b193b321b70cb422e21d368336d0743f365f1301
Derek <derlloyd@outlook.com>
2016-04-14 20:52:17 +0000

    warrior exercise

---
+++ warriors.js
@@ -0,0 +1,27 @@
+function Warrior(name, gender) {
+    this.name = name,
+    this.gender = gender,
+    this.level = 1,
+    this.weapon = "wooden sword",
+    this.power = Math.ceil(Math.random()*100)
+}
+
+Warrior.prototype = {
+    fight: function() {console.log("rushes to the arena with")},
+    faceoff: function(opponent) {
+        if (this.power >= opponent.power) {
+            console.log(this.name + " defeats " + opponent.name + " with " 
+            + this.power + " versus " + opponent.power + " power!");
+        } else {
+            console.log(opponent.name + " defeats " + this.name + " with " 
+            + opponent.power + " versus " + this.power + " power!");            
+        }
+    }
+}
+
+var warriorA = new Warrior("Dave","M")
+var warriorB = new Warrior("Grohl", "M")
+var WarriorC = new Warrior("Susan", "F")
+
+console.log(warriorB.fight())
+console.log(warriorA.faceoff(warriorB))
